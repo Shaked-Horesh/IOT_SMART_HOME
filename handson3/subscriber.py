be2996f2723fed870619c8fad0a6a7a45b21ec89
Shaked-Horesh <shakedholearn@gmail.com>
2026-03-05 10:53:59 +0200

    Update MQTT connection settings and callbacks

--- handson3/subscriber.py
+++ handson3/subscriber.py
@@ -1,39 +1,36 @@
 import time
 import paho.mqtt.client as mqtt
 
-HOST = "139.162.222.115"
-PORT = 443
-USERNAME = "MATZI"
-PASSWORD = "MATZI"
+HOST = "test.mosquitto.org"  #ציבורי
+PORT = 1883
+USERNAME = None
+PASSWORD = None
 
-TOPIC = "hit/iot/shaked/test"   # אפשר לשנות, אבל להשאיר עקבי בין שניהם
+TOPIC = "hit/iot/shakedhoresh/handson3/demo1"
 
-def on_connect(client, userdata, flags, rc):
-    # rc == 0 זה הצלחה
-    print("CONNECTED, rc=", rc)
-    if rc == 0:
+def on_connect(client, userdata, flags, reason_code, properties=None):
+    print("CONNECTED, rc=", reason_code)
+    if reason_code == 0:
         client.subscribe(TOPIC, qos=1)
         print("SUBSCRIBED to:", TOPIC)
 
 def on_message(client, userdata, msg):
     print(f"RECEIVED topic={msg.topic} qos={msg.qos} payload={msg.payload.decode(errors='ignore')}")
 
-def on_disconnect(client, userdata, rc):
-    print("DISCONNECTED, rc=", rc)
+def on_disconnect(client, userdata, disconnect_flags, reason_code, properties=None):
+    print("DISCONNECTED, rc=", reason_code)
 
 client_id = f"shaked_sub_{int(time.time())}"
 
-client = mqtt.Client(client_id=client_id, transport="websockets")  # חשוב ל-443
-client.username_pw_set(USERNAME, PASSWORD)
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=client_id)
+
+if USERNAME and PASSWORD:
+    client.username_pw_set(USERNAME, PASSWORD)
 
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_disconnect = on_disconnect
 
-# אם אצלכם צריך path אחר ל-websocket, לפעמים זה "/mqtt"
-# client.ws_set_options(path="/mqtt")
-
-print("Connecting...")
+print(f"Connecting to {HOST}:{PORT} ...")
 client.connect(HOST, PORT, keepalive=60)
-
 client.loop_forever()
